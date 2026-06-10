b1e3d8f0def13fc7969d6a97cbccccf0f8c7a6b0
vynn122 <hemtheavin@gmail.com>
2026-06-10 16:22:07 +0700

    finish set up

---
+++ boot_keyboardApp_server.ps1
@@ -0,0 +1,10 @@
+# Navigate to project
+Set-Location "C:\Users\ahpka\Desktop\python-with-django\macer"
+
+# Navigate to VM and activate it
+& "C:\Users\ahpka\Desktop\python-with-django\macer\virtualEnv\Scripts\Activate.ps1"
+
+
+# Activate server
+py -m manage runserver
+
---
+++ boot_keyboardApp_server_kimly.ps1
@@ -0,0 +1,9 @@
+cd "D:\Django Team Project\macer"
+
+# Navigate to VM and activate it
+& "D:\Django Team Project\macer\env\Scripts\Activate.ps1"
+
+
+# Activate server
+py -m manage runserver
+

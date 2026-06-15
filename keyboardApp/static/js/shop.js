21d2936946d46d23e682f21eecb4e8095cdb8c2c
vynn122 <hemtheavin@gmail.com>
2026-06-16 00:10:43 +0700

    change all baseUrl and add csrf trust origin

--- keyboardApp/static/js/shop.js
+++ keyboardApp/static/js/shop.js
@@ -5,6 +5,7 @@ let cartRenderArr = [];
 let queryParameter = new URLSearchParams(window.location.search);
 let item_price = 0;
 let CurrentUser = "";
+let baseUrl = "http://3.25.246.176:8000";
 const getCSRFToken = () => {
   return document.querySelector("[name=csrfmiddlewaretoken]").value;
 };

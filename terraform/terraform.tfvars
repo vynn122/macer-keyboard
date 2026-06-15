1dd09bb1dc74df8e502edd5dbbb49aa508760f83
vynn122 <hemtheavin@gmail.com>
2026-06-15 23:33:25 +0700

    set up terraformm and fix baseurl in signup

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region       = "ap-southeast-2"
+instance_name    = "Macer Web Server"
+instance_type    = "t3.micro"
+key_name         = "macer key"
+root_volume_size = 8
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,40 @@
-variable "region" {
-  default = "ap-southeast-2"
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "my-ec2-instance"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use. If left empty, the latest Amazon Linux 2023 AMI is used."
+  type        = string
+  default     = ""
+}
+
+variable "key_name" {
+  description = "Name of an existing EC2 key pair for SSH access"
+  type        = string
+}
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed to SSH into the instance (lock this down to your IP, e.g. 1.2.3.4/32)"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "root_volume_size" {
+  description = "Size of the root EBS volume in GB"
+  type        = number
+  default     = 20
 }

1e645a063c86b54526fcee9a310920d63d252768
vynn122 <hemtheavin@gmail.com>
2026-06-16 19:45:37 +0700

    fix setting

--- src/settings.py
+++ src/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--%hq+oo&&1neciwqtk+vzzj&itaed&*g_apjr43k_nzq!ptz$)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
